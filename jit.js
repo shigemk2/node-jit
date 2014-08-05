12f424fd176f15fe9d966f79132b83428690aa8b
shigemk2 <i.am.shige@gmail.com>
2014-08-05 13:17:26 +0900

    Unite platform

--- jit.js
+++ jit.js
@@ -1,36 +1,75 @@
-ref = require("ref")
-ffi = require("ffi")
+var ref = require("ref");
+var ffi = require("ffi");
+var os  = process.platform;
 
-pVirtualAlloc = ffi.ForeignFunction(
-    ffi.DynamicLibrary("kernel32").get("VirtualAlloc"),
-    "pointer", ["pointer", "size_t", "int", "int"])
-function VirtualAlloc(address, size, allocationType, protect) {
-    return pVirtualAlloc(address, size, allocationType, protect).
-        reinterpret(size)
-}
-VirtualFree = ffi.ForeignFunction(
-    ffi.DynamicLibrary("kernel32").get("VirtualFree"),
-    "bool", ["pointer", "int", "int"])
-MEM_COMMIT  = 0x1000
-MEM_RELEASE = 0x8000
-PAGE_EXECUTE_READWRITE = 0x40
+var jitalloc = (function() {
+    if (os == "win32") {
+        var kernel32 = ffi.Library("kernel32", {
+            "VirtualAlloc": ["pointer", ["pointer", "size_t", "int", "int"]],
+            "VirtualFree": ["bool", ["pointer", "int", "int"]],
+        });
+        
+        var MEM_COMMIT  = 0x1000;
+        var MEM_RELEASE = 0x8000;
+        var PAGE_EXECUTE_READWRITE = 0x40;
+        
+        return function(size) {
+            var p = kernel32.VirtualAlloc(ref.NULL, size,
+                                          MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+            var ret = p.reinterpret(size);
+            ret.free = function() {
+                kernel32.VirtualFree(p, 0, MEM_RELEASE);
+            };
+            return ret;
+        };
+    } else {
+        var libc = ffi.Library("libc", {
+            "mmap": ["pointer", ["pointer", "size_t", "int", "int", "int", "int64"]],
+            "munmap": ["int", ["pointer", "size_t"]],
+        });
+        
+        var PROT_READ   = 1;
+        var PROT_WRITE  = 2;
+        var PROT_EXEC   = 4;
+        var MAP_PRIVATE = 2;
+        var MAP_ANON    = os == "linux" || os == "linux2" ? 0x20 : 0x1000;
+        
+        return function(size) {
+            var p = libc.mmap(ref.NULL, size,
+                              PROT_READ | PROT_WRITE | PROT_EXEC,
+                              MAP_PRIVATE | MAP_ANON, -1, 0);
+            var ret = p.reinterpret(size);
+            ret.free = function() {
+                libc.munmap(p, size);
+            };
+            return ret;
+        };
+    }
+})();
 
-buf = VirtualAlloc(ref.NULL, 16, MEM_COMMIT, PAGE_EXECUTE_READWRITE)
+var buf = jitalloc(16);
+
 if (ref.sizeof.pointer == 4) {
     // 32bit (i386)
     buf.binaryWrite(
         "\x8b\x44\x24\x04" +    // mov eax, [esp+4]
         "\x03\x44\x24\x08" +    // add eax, [esp+8]
-        "\xc3", 0)              // ret
-} else {
-    // 64bit (x86-64)
+        "\xc3", 0);             // ret
+} else if (os == "win32") {
+    // 64bit (x86-64) Windows
     buf.binaryWrite(
         "\x89\xc8" +            // mov eax, ecx
         "\x01\xd0" +            // add eax, edx
         "\xc3", 0)              // ret
+} else {
+    // 64bit (x86-64) System V ABI
+    buf.binaryWrite(
+        "\x89\xf8" +            // mov eax, edi
+        "\x01\xf0" +            // add eax, esi
+        "\xc3", 0);             // ret
 }
 
-func = ffi.ForeignFunction(buf, "int", ["int", "int"])
-console.log(func(1, 2))
+var func = ffi.ForeignFunction(buf, "int", ["int", "int"]);
+console.log(func(1, 2));
 
-VirtualFree(buf, 0, MEM_RELEASE)
+buf.free();
